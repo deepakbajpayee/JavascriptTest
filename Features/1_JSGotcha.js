6fd2d7e85d229ce6a952b2ba117dd900bf18e93a
Deepak Kumar <deepakbajpayee47@gmail.com>
2024-01-02 20:52:20 +0530

    rereading rediscovering javascript

--- Features/1_JSGotcha.js
+++ Features/1_JSGotcha.js
@@ -1,43 +1,48 @@
 /**
- * 1. Javascript automatically inserts a ;.
+ * 1. Javascript automatically inserts a ';'
  */
 
 
 'use strict';
 /*
-Benefits of "use strict".
+"use strict".
     1. Catches silent errors: Assigning values to an undeclared variable.
     2. Forces parameter name uniqueness.
-    3. Watches changes to read-only properties, deletion of properties.
-    4. Enhances security, etc.
+    3. Assigning a value to a read-only property, like undefined or NaN, will throw an error.
+    4. Enhances security, etc..
+*/
 
+
+/*
 Install and Set EsLint to better watch out for issues.
     1. npm install -g eslint
     2. eslint --init
     3. eslint Gotcha.js
  */
 
-const Gotchas1 = function (){
+const Gotchas1 = function () {
     const a = '1';
     const b = 1;
     const c = '1.0';
 
     /**
-     * The == operator performs type coercion: if things being compared
-     * are not of the same type
+     * The == operator performs type coercion: 
+     * if things being compared are not of the same type.
+     * 
+     * === : strict equality.
      */
-    console.log(a == b);
-    console.log(b == c);
-    console.log(a == c);
+    console.log(a === b);
+    console.log(b === c);
+    console.log(a === c); // lexical comparison.
 }
 
 
 const Gotchas2 = function (){
-    //haha = 2; // becomes global.
-    let haha = 2;
+    haha = 2; // becomes global. Declare variable before use.
+    //let haha = 2;
     console.log(haha);
 }
 
-//Gotchas1();
+Gotchas1();
 Gotchas2();
 //console.log(haha);
--- Features/2_Variables.js
+++ Features/2_Variables.js
@@ -3,9 +3,9 @@
 /*
   var does two things poorly:
    1. Doesn't prevent a variable from being redefined in a scope.
-   2. Does not have a block scope.
+   2. Does not have a block scope. Variables defined(with var) within functions have function scope.
 
-   Variables defined(with var) within functions have function scope.
+   var hoists the variable to the top of the function.
  */
 
 /*
@@ -13,12 +13,10 @@
     1. Let does not permit a variable in scope to be redefined.
     2. Block scope {..}.
     3. Variables are not hoisted to the top of the function.
- */
 
+    If a function will have access to variables defined outside its immediate scope, 
+    it's better to make those variables immutable.
 
-/**
- * If a function will have access to variables defined outside its immediate
- * scope, it's better to make those variables immutable.
  */
 
 let factor = 2;
@@ -26,13 +24,13 @@ let product = function (number){
     return number * factor;
 }
 factor = 0;
-console.log(product(4));
+console.log(product(4)); // prints 0. Not captured at the time of definition.
 
 /*
     Const.
     1. Only primitive values, like number, and references to objects are
-    protected from change. The actual object that the reference refers to
-    does not receive any protection.
+    protected from change. 
+    2. The actual object that the reference refers to does not receive any protection.
 
  */
 
@@ -46,15 +44,31 @@ const sam = { first: 'Sam', age: 2 };
 //nothing = undefined; //Not allowed
 //nope = null; //Not allowed
 //sam = { first: 'Sam', age: 2 }; //Not allowed
-sam.age = 3;
+sam.age = 3; //allowed
 console.log(sam.age);
+
+
 
 /**
- * Shallow Freeze: Makes only top-level properties read-only.
- * A code that fails loudly is better than one that misbehaves quietly.
- */
+  To make a reference immutable, use const. However, to make an object itself immutable.
 
-const sam = Object.freeze({ first: 'Sam', age: 2 });
-//sam = {}; //ERROR, the reference sam is immutable
-sam.age = 3; // Error.
-console.log(sam.age);
+ Shallow Freeze: 
+    1. Object.freeze(): Returns a reference to an object that is immutable. 
+    2. Makes only top-level properties read-only.
+    3. A code that fails loudly is better than one that misbehaves quietly. (use strict)
+
+*/
+
+const greet = 'dude';
+console.log(greet);
+//greet[0] = 'r'; // Error: Cannot assign to read only property '0' of string 'dude'
+
+
+const sam1 = Object.freeze({ first: 'Sam', age: 2 });
+//sam1 = {}; //ERROR, the reference sam is immutable
+//sam1.age = 3; // Error.
+console.log(sam1.age);
+
+/**
+ IIFE Design Pattern: to prevent variable hoisting.
+ */
